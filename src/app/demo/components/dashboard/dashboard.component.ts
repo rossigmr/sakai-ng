57596dfffdeaa9e500778f0c799d0ecf4b0ef248
rossigmr <129901369+rossigmr@users.noreply.github.com>
2024-02-05 22:10:21 -0300

    primeiro commit

--- src/app/demo/components/dashboard/dashboard.component.ts
+++ src/app/demo/components/dashboard/dashboard.component.ts
@@ -5,6 +5,7 @@ import { ProductService } from '../../service/product.service';
 import { Subscription, debounceTime } from 'rxjs';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
+
 @Component({
     templateUrl: './dashboard.component.html',
 })
